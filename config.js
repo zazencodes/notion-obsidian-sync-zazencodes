bde8af0c3bcd329296f56dddb3339b6bdb58eb05
Alexander Galea <agalea91@gmail.com>
2024-03-06 22:48:24 -0500

    Add folder databases

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 const config = {
   obsidianVault: "/Users/alex/library/Mobile Documents/iCloud~md~obsidian/Documents/ZazenCodes/notes",
   noteTagIds: [
+    { folder: "book", databaseId: "b403753ce0c049e48c83ff2c3d896843" },
+    { folder: "blog", databaseId: "1f9ab538edc04cb19e876653040b761f" },
+    { folder: "course", databaseId: "ccf45222ea024f3795d2df10340afcef" },
+    { folder: "doc", databaseId: "4193377febff45b1a6cc587df87bf424" },
+    { folder: "fact", databaseId: "49b474595a4b4893b3f23d68a2ad4709" },
     { folder: "resource", databaseId: "de8db5301bb44491a807a72d0775fb2e" },
     { folder: "video", databaseId: "46af621096a145908bb04054629ee072" },
   ]

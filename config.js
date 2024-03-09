1bc56bbb8cc45e2a0827d3d62a7e4da8ff1d7543
Alexander Galea <agalea91@gmail.com>
2024-03-09 14:23:42 -0500

    Update dbs

--- config.js
+++ config.js
@@ -8,6 +8,9 @@ const config = {
     { folder: "fact", databaseId: "49b474595a4b4893b3f23d68a2ad4709" },
     { folder: "resource", databaseId: "de8db5301bb44491a807a72d0775fb2e" },
     { folder: "video", databaseId: "46af621096a145908bb04054629ee072" },
+    { folder: "cheat-sheet", databaseId: "9fd1106079a94a76a3834472bc555fd6" },
+    { folder: "code-snip", databaseId: "445273a8fd1e43b2a4c1e8a0adda0a28" },
+
   ]
 }
 
